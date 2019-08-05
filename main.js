717df776e9743ada09eceafeaa4ed59b46a6c68f
OPenOgboluASC <Obedpogbolu@gmail.com>
2019-08-05 10:58:48 -0400

    Giphy Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const images = document.getElementById("images");
+
+const search = document.getElementById("search");
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', function() {
+    images.innerHTML = "";
+    getData(search.value);
+});
+
+function getData(query){
+    fetch('http://api.giphy.com/v1/gifs/search?q='+query+'&api_key=AWPmNeZQjU5gVEjv9qGiaCOl3PnvX63W&limit=5')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        renderData(myJson);
+        //console.log(JSON.stringify(myJson));
+    });
+}
+
+function renderData(data) {
+    console.log(data.data);
+    for(let i = 0; i < data.data.length; i++){
+        const img = document.createElement('img');
+        img.src = data.data[i].images.original.url;
+
+        images.appendChild(img)
+    }
+}
+
+getData();
